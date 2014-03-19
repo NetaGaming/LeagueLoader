41ba68d81223e6d3105263b8b44c05e51f7120ed
Matthew Cantelon <me@matthewcantelon.ca>
2014-03-19 06:47:55 -0700

    Updated to take a better approach to downloading game data
    Previously, games.go would only look at game IDs that were
    in `game_info` table. This was to make updating faster but
    ran the risk of not all stats being downloaded for all players.
    Recently ran into this problem; two people were missed on an update
    (likely because the game ended just before the loader ran) and then
    could not be picked back up as the game was already in `game_info`.
    
    There is now a seperate check to grab all the played games for each
    summoner from `summoner_games` and a check to make sure games only
    go into `game_info` once.

--- games.go
+++ games.go
@@ -6,56 +6,135 @@ import (
 	"github.com/coopernurse/gorp"
 )
 
+type PlayedGame struct {
+	SummonerId int64 `db:"summonerId"`
+	GameId     int64 `db:"gameId"`
+}
+
+func hasSummonerAlreadyPlayedGame(summonerId int64, gameId int64, games []PlayedGame) (gameIsPlayed bool) {
+	for _, game := range games {
+		if game.SummonerId == summonerId && game.GameId == gameId {
+			return true
+		}
+	}
+
+	return false
+}
+
 // use list of summoners to download game data
 func updateGames(summoners []int64, dbmap *gorp.DbMap) (gameCount int) {
 
 	// get stored game ids for summoners
-	var gameIdQuery string = `SELECT id FROM  game_info`
+	var summonerGameIdQuery string = `
+        SELECT gameId
+        FROM summoner_games
+        INNER JOIN game_info gi ON
+            gi.id = gameId
+        WHERE summonerId = %d
+        ORDER by gi.date desc`
+	var gameIdQuery string = "SELECT id FROM game_info"
 	var gameIds []int64 = make([]int64, 1)
 	_, err := dbmap.Select(&gameIds, gameIdQuery)
 	checkErr(err, "Could not get game ids from database")
 
 	// get most recent games for each summoner
+	var playedGames []int64 = make([]int64, 1)
 	var savedGames = make([]int64, 1)
 	for _, summonerId := range summoners {
+		fmt.Printf("\nChecking summoner: %d\n", summonerId)
+		fmt.Printf("---\n")
 		summonerGames, riotErr := goriot.RecentGameBySummoner(goriot.NA, summonerId)
 		if riotErr != nil {
 			panic(riotErr)
-		} else {
+		}
 
-			// save game if we don't already have it
-			for _, game := range summonerGames {
+		_, err := dbmap.Select(
+			&playedGames,
+			fmt.Sprintf(summonerGameIdQuery, summonerId))
+		checkErr(err, "Could not find games for summoner")
 
-				if existsInSlice(game.GameID, gameIds) == false {
+		fmt.Printf("%d has %d games played\n", summonerId, len(playedGames))
 
-					if existsInSlice(game.GameID, savedGames) == false {
+		// save game if we don't already have it
+		for _, game := range summonerGames {
 
-						fmt.Println(game.CreateDate)
-						// save game to db
-						var gameInfoQuery string = `
+			if existsInSlice(game.GameID, gameIds) == false {
+
+				if existsInSlice(game.GameID, savedGames) == false {
+
+					updateGameInfo(game, dbmap)
+
+					// save game id to list to skip loading into
+					// game_info, in the event it shows again
+					savedGames = append(savedGames, game.GameID)
+				}
+			}
+			if existsInSlice(game.GameID, playedGames) == false {
+				fmt.Printf("%d hasn't played %d\n", summonerId, game.GameID)
+				var stats goriot.GameStat = game.Statistics
+				statId := updateSummonerStatistics(summonerId, stats, dbmap)
+
+				updateSummonerGames(
+					summonerId, game, statId, stats.Win, dbmap)
+			}
+		}
+
+	}
+
+	return len(savedGames)
+}
+
+func updateGameInfo(game goriot.Game, db *gorp.DbMap) {
+	// save game to db
+	var gameInfoQuery string = `
                             INSERT INTO game_info
                                 (id, mode, type, subType, mapId, date)
                             VALUES
                                 (?, ?, ?, ?, ?, FROM_UNIXTIME(?))`
-						_, infoErr := dbmap.Exec(
-							gameInfoQuery,
-							game.GameID,
-							game.GameMode,
-							game.GameType,
-							game.SubType,
-							game.MapID,
-							game.CreateDate/1000)
-						if infoErr != nil {
-							panic(infoErr)
-						}
+	_, infoErr := db.Exec(
+		gameInfoQuery,
+		game.GameID,
+		game.GameMode,
+		game.GameType,
+		game.SubType,
+		game.MapID,
+		game.CreateDate/1000)
+	if infoErr != nil {
+		panic(infoErr)
+	}
+}
 
-						// save game id to list to skip loading into
-						// game_info, in the event it shows again
-						savedGames = append(savedGames, game.GameID)
-					}
-					var stats goriot.GameStat = game.Statistics
-					// save stats
-					var statsQuery string = `
+func updateSummonerGames(
+	summonerId int64,
+	game goriot.Game,
+	statId int64,
+	wonGame bool,
+	db *gorp.DbMap) {
+	// save summoner_game
+	var summonerGameQuery string = `
+                        INSERT INTO summoner_games
+                            (summonerId, gameId, championId, spellOne, spellTwo,
+                             statId, won)
+                        VALUES
+                            (?,?,?,?,?,?,?)`
+	_, sgErr := db.Exec(
+		summonerGameQuery,
+		summonerId,
+		game.GameID,
+		game.ChampionID,
+		game.Spell1,
+		game.Spell2,
+		statId,
+		wonGame)
+	checkErr(sgErr, "Could not save summoner game info")
+}
+
+func updateSummonerStatistics(
+	summonerId int64,
+	stats goriot.GameStat,
+	db *gorp.DbMap) (statId int64) {
+	// save stats
+	var statsQuery string = `
                         INSERT INTO summoner_stats
                             (assists, barracksKilled, championsKilled,
                              combatPlayerScore, consumablesPurchased,
@@ -87,110 +166,87 @@ func updateGames(summoners []int64, dbmap *gorp.DbMap) (gameCount int) {
                             (?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,
                              ?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,
                              ?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?)`
-					statRes, statErr := dbmap.Exec(
-						statsQuery,
-						stats.Assists,
-						stats.BarracksKilled,
-						stats.ChampionsKilled,
-						stats.CombatPlayerScore,
-						stats.ConsumablesPurchased,
-						stats.DamageDealtPlayer,
-						stats.DoubleKills,
-						stats.FirstBlood,
-						stats.Gold,
-						stats.GoldEarned,
-						stats.GoldSpent,
-						stats.Item0,
-						stats.Item1,
-						stats.Item2,
-						stats.Item3,
-						stats.Item4,
-						stats.Item5,
-						stats.Item6,
-						stats.ItemsPurchased,
-						stats.KillingSprees,
-						stats.LargestCriticalStrike,
-						stats.LargestKillingSpree,
-						stats.LargestMultiKill,
-						stats.LegendaryItemsCreated,
-						stats.Level,
-						stats.MagicDamageDealtPlayer,
-						stats.MagicDamageDealtToChampions,
-						stats.MagicDamageTaken,
-						stats.MinionsDenied,
-						stats.MinionsKilled,
-						stats.NeutralMinionsKilled,
-						stats.NeutralMinionsKilledEnemyJungle,
-						stats.NeutralMinionsKilledYourJungle,
-						stats.NexusKilled,
-						stats.NodeCapture,
-						stats.NodeCaptureAssist,
-						stats.NodeNeutralize,
-						stats.NodeNeutralizeAssist,
-						stats.NumDeaths,
-						stats.NumItemsBought,
-						stats.ObjectivePlayerScore,
-						stats.PentaKills,
-						stats.PhysicalDamageDealtPlayer,
-						stats.PhysicalDamageDealtToChampions,
-						stats.PhysicalDamageTaken,
-						stats.QuadraKills,
-						stats.SightWardsBought,
-						stats.Spell1Cast,
-						stats.Spell2Cast,
-						stats.Spell3Cast,
-						stats.Spell4Cast,
-						stats.SummonSpell1Cast,
-						stats.SummonSpell2Cast,
-						stats.SuperMonsterKilled,
-						stats.Team,
-						stats.TeamObjective,
-						stats.TimePlayed,
-						stats.TotalDamageDealt,
-						stats.TotalDamageDealtToChampions,
-						stats.TotalDamageTaken,
-						stats.TotalHeal,
-						stats.TotalPlayerScore,
-						stats.TotalScoreRank,
-						stats.TotalTimeCrowdControlDealt,
-						stats.TotalUnitsHealed,
-						stats.TripleKills,
-						stats.TrueDamageDealtPlayer,
-						stats.TrueDamageDealtToChampions,
-						stats.TrueDamageTaken,
-						stats.TurretsKilled,
-						stats.UnrealKills,
-						stats.VictoryPointTotal,
-						stats.VisionWardsBought,
-						stats.WardKilled,
-						stats.WardPlaced)
-					checkErr(statErr, "Could not insert stats")
+	statRes, statErr := db.Exec(
+		statsQuery,
+		stats.Assists,
+		stats.BarracksKilled,
+		stats.ChampionsKilled,
+		stats.CombatPlayerScore,
+		stats.ConsumablesPurchased,
+		stats.DamageDealtPlayer,
+		stats.DoubleKills,
+		stats.FirstBlood,
+		stats.Gold,
+		stats.GoldEarned,
+		stats.GoldSpent,
+		stats.Item0,
+		stats.Item1,
+		stats.Item2,
+		stats.Item3,
+		stats.Item4,
+		stats.Item5,
+		stats.Item6,
+		stats.ItemsPurchased,
+		stats.KillingSprees,
+		stats.LargestCriticalStrike,
+		stats.LargestKillingSpree,
+		stats.LargestMultiKill,
+		stats.LegendaryItemsCreated,
+		stats.Level,
+		stats.MagicDamageDealtPlayer,
+		stats.MagicDamageDealtToChampions,
+		stats.MagicDamageTaken,
+		stats.MinionsDenied,
+		stats.MinionsKilled,
+		stats.NeutralMinionsKilled,
+		stats.NeutralMinionsKilledEnemyJungle,
+		stats.NeutralMinionsKilledYourJungle,
+		stats.NexusKilled,
+		stats.NodeCapture,
+		stats.NodeCaptureAssist,
+		stats.NodeNeutralize,
+		stats.NodeNeutralizeAssist,
+		stats.NumDeaths,
+		stats.NumItemsBought,
+		stats.ObjectivePlayerScore,
+		stats.PentaKills,
+		stats.PhysicalDamageDealtPlayer,
+		stats.PhysicalDamageDealtToChampions,
+		stats.PhysicalDamageTaken,
+		stats.QuadraKills,
+		stats.SightWardsBought,
+		stats.Spell1Cast,
+		stats.Spell2Cast,
+		stats.Spell3Cast,
+		stats.Spell4Cast,
+		stats.SummonSpell1Cast,
+		stats.SummonSpell2Cast,
+		stats.SuperMonsterKilled,
+		stats.Team,
+		stats.TeamObjective,
+		stats.TimePlayed,
+		stats.TotalDamageDealt,
+		stats.TotalDamageDealtToChampions,
+		stats.TotalDamageTaken,
+		stats.TotalHeal,
+		stats.TotalPlayerScore,
+		stats.TotalScoreRank,
+		stats.TotalTimeCrowdControlDealt,
+		stats.TotalUnitsHealed,
+		stats.TripleKills,
+		stats.TrueDamageDealtPlayer,
+		stats.TrueDamageDealtToChampions,
+		stats.TrueDamageTaken,
+		stats.TurretsKilled,
+		stats.UnrealKills,
+		stats.VictoryPointTotal,
+		stats.VisionWardsBought,
+		stats.WardKilled,
+		stats.WardPlaced)
+	checkErr(statErr, "Could not insert stats")
 
-					statId, statIdErr := statRes.LastInsertId()
-					checkErr(statIdErr, "Could not get last insterted id")
+	statId, statIdErr := statRes.LastInsertId()
+	checkErr(statIdErr, "Could not get last insterted id")
 
-					// save summoner_game
-					var summonerGameQuery string = `
-                        INSERT INTO summoner_games
-                            (summonerId, gameId, championId, spellOne, spellTwo,
-                             statId, won)
-                        VALUES
-                            (?,?,?,?,?,?,?)`
-					_, sgErr := dbmap.Exec(
-						summonerGameQuery,
-						summonerId,
-						game.GameID,
-						game.ChampionID,
-						game.Spell1,
-						game.Spell2,
-						statId,
-						stats.Win)
-					checkErr(sgErr, "Could not save summoner game info")
-				}
-			}
-
-		}
-	}
-
-	return len(savedGames)
+	return statId
 }
